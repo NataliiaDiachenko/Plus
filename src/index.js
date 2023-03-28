118ce752f1bdacbfe58294940ea47cf1b953e877
NataliiaDiachenko <117234112+NataliiaDiachenko@users.noreply.github.com>
2023-03-29 00:32:11 +0300

    add

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="22"
+          width="30"
         />
           <div class="weather-forecast-temperatures">
                     <span class="weather-forecast-temperatures-max">${Math.round(
