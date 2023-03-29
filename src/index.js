05cee69fbb6afd6eb1dfe834fbbbe52aa713feb5
NataliiaDiachenko <117234112+NataliiaDiachenko@users.noreply.github.com>
2023-03-29 23:24:53 +0300

    add

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  return `${day} </br>
+  ${hours}:${minutes}`;
+  
 }
 
 function formatDay(timestamp) {
@@ -90,17 +92,14 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
+  iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
 }
 
 function search(city) {
-  let apiKey = "96f79b8dee1c030cc01912ef253094a8";
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
